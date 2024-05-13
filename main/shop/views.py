3c837653a5487659c771c3fb1ace67576787e7ac
sanchyopas <saniagolovanev@gmail.com>
2024-05-13 15:59:51 +0700

    FIX

--- main/shop/views.py
+++ main/shop/views.py
@@ -36,7 +36,7 @@ def category(request):
     if li.char_value not in chars_list_name_noduble:
       chars_list_name_noduble.append(li.char_value)
   
-  chars = ProductChar.objects.filter(char_value__in=chars_list_name_noduble).distinct('char_value')
+  chars = ProductChar.objects.filter(char_value__in=chars_list_name_noduble).values('char_value').distinct()
   
   chars_list_name_noduble_a = ProductChar.objects.filter(parent__in=products_all).distinct().values_list('char_value', flat=True).distinct()
   # print(chars_list_name_noduble_a)

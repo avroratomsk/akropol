0915582f4a1750dfc05b48816c3d7eb744f706e9
sanchyopas <saniagolovanev@gmail.com>
2024-09-10 15:13:45 +0700

    FIX

--- main/home/views.py
+++ main/home/views.py
@@ -117,7 +117,7 @@ def index(request):
     home_page = HomeTemplate.objects.all()
 
   category = Category.objects.all()[:4]
-  products = Product.objects.filter(status=True).prefetch_related('chars')[:8]
+  products = Product.objects.filter(status=True, catalog=True).prefetch_related('chars')[:8]
   reviews = Reviews.objects.filter(status=True)
   gallery = Gallery.objects.filter(is_active=True)
   category_gallery = GalleryCategory.objects.filter(home_view=True)

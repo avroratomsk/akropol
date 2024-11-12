825797a757b6ed39b872f55959c9d49e745396ec
sanchyopas <saniagolovanev@gmail.com>
2024-11-12 10:37:16 +0300

    FIX

--- main/shop/views.py
+++ main/shop/views.py
@@ -71,7 +71,7 @@ def category_detail(request, slug):
 
 def product(request, slug):
   product = Product.objects.get(slug=slug)
-  products = Product.objects.filter(category_slug="soputstvuyushchie-tovary")
+  products = product.get_related_products()
   product_color = ColorProduct.objects.filter(active=True)
   images = ProductImage.objects.filter(parent_id=product.id)[:3]
   chars_all = ProductChar.objects.filter(parent=product).distinct()

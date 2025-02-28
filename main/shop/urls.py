816b00aaff73961a0530a9308459458f5a84668f
sanchyopas <saniagolovanev@gmail.com>
2025-02-28 10:37:01 +0700

    fix

--- main/shop/urls.py
+++ main/shop/urls.py
@@ -4,9 +4,11 @@ from shop import views
 
 
 urlpatterns = [
+    #Если я сделаю данный маршрут ниже чем catalog_detail, то поиск не будет
+    #работать по той причине что всегда будет выаолняться маршрут в который передали slug
+
     path('search/', views.category_detail, name="search"), 
     path('', views.category, name="category"),
-    #Если я сделаю данный маршрут ниже чем catalog_detail, то поиск не будет работать по той причине что всегда будет выаолняться маршрут в который передали slug
     path('<slug:slug>/', views.category_detail, name="category_detail"),
     path('product/<slug:slug>/', views.product, name="product"),
     
